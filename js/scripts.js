c69d5afa1da4da6475943af26f7c3a820f96ee91
Jarrod Green and Jordan King <jordan@>
2019-07-08 20:29:59 -0700

    added function to js that calculates total points for each turn

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,25 @@ $(document).ready(function() {
     function getRandomInt() {
       return Math.floor((Math.random() * 6) + 1);
     }
-    // console.log(getRandomInt());
+
     $("#rollresult").empty();
     userTotal.push(getRandomInt())
 
-    var total = 0;
+    var rollLog = 0;
       for(var index = 0; index < userTotal.length; index += 1) {
         $("#rollresult").text(userTotal[index]);
       }
-      console.log(userTotal);
+
+    var rollTotal = 0
+      for(var index = 0; index < userTotal.length; index += 1) {
+        rollTotal += userTotal[index];
+      }
+
+    $("#turnTotal").empty();  
+    $("#turnTotal").text(rollTotal);
+
+
+      console.log(rollTotal);
 
 
 
