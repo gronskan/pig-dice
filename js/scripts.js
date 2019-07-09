9553d68077597670617b1458716c3969e9d15c03
Jarrod Green and Jordan King <jarrod@>
2019-07-08 20:19:46 -0700

    Fixed js function to push matching number rolled into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
+
+  var userTotal = []
+
   $("#roll").click(function(event){
     event.preventDefault();
 
     function getRandomInt() {
       return Math.floor((Math.random() * 6) + 1);
     }
-    console.log(getRandomInt());
+    // console.log(getRandomInt());
     $("#rollresult").empty();
-    $("#rollresult").text(getRandomInt());
+    userTotal.push(getRandomInt())
+
+    var total = 0;
+      for(var index = 0; index < userTotal.length; index += 1) {
+        $("#rollresult").text(userTotal[index]);
+      }
+      console.log(userTotal);
 
 
 
-  })
+    // userTotal.push(parseInt("#rollresult"));
+    //     console.log("#rollresult");
+  });
+
+
+    // console.log(userTotal);
 })
