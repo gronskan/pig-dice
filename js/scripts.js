536a0b5c0cc2f862df296ab989acc3f445f25643
Jarrod Green and Jordan King <jarrod@>
2019-07-10 20:36:34 -0700

    updated logic for scoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,75 @@
+function Player () {
+  this.currentRoll
+  this.turnTotal
+  this.totalScore
+}
+
+var player1 = new Player
+var player2 = new Player
+
+player1Turn = true
+
+
+
 $(document).ready(function() {
 
-  var userTotal = []
+  var currentRoll = []
 
   $("#roll").click(function(event){
     event.preventDefault();
-
+    if (player1Turn === true) {
     function getRandomInt() {
       return Math.floor((Math.random() * 6) + 1);
     }
 
     $("#rollresult").empty();
     $("#turnTotal").empty();
-    userTotal.push(getRandomInt())
+    currentRoll.push(getRandomInt())
+    player1.turnTotal += rollTotal
+    } else  {
+      function getRandomInt() {
+        return Math.floor((Math.random() * 6) + 1);
+      }
+
+      $("#rollresult").empty();
+      $("#turnTotal").empty();
+      currentRoll.push(getRandomInt())
+      player2.turnTotal += rollTotal
+
+
+  }
 
 
 
     var rollTotal = 0
-      for(var index = 0; index < userTotal.length; index += 1) {
-        $("#rollresult").text(userTotal[index]);
-        rollTotal += userTotal[index];
+      for(var index = 0; index < currentRoll.length; index += 1) {
+        $("#rollresult").text(currentRoll[index]);
+        if (currentRoll[index] === 1) {
+          rollTotal -= rollTotal;
+          $("#turnOver").show();
+          $("#turnContinue").hide();
+        } else {
+          rollTotal += currentRoll[index];
+          console.log(rollTotal);
+        };
+      };
+
+      $("#hold").click(function() {
+        if (player1Turn === true) {
+          player1.totalScore += rollTotal
+        $("#player1Score").text(player1.totalScore);
+      } else {
+        player2.totalScore += rollTotal
+        $("#player2Score").text(player2.totalScore);
+        player1Turn = !player1Turn
       }
+      });
 
 
     $("#turnTotal").text(rollTotal);
 
 
-      console.log(rollTotal);
-
-
-
-    // userTotal.push(parseInt("#rollresult"));
-    //     console.log("#rollresult");
   });
-
 
     // console.log(userTotal);
 })
